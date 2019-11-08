602794a280f67cdaa94522d52c066bd9225c4d37
lquezada <que18028@uvg.edu.gt>
2019-11-08 07:36:04 -0600

    letters

--- RSA.py
+++ RSA.py
@@ -5,14 +5,15 @@
 #Esteban del Valle  18221
 #Luis Quezada		18028
 
-from random import *
+import random
 
-def primalidad (num):
-    for i in range (2,(num-1)):
-        if ((num%i)==0):
-            return False
-            break   
+def isPrime(num):
+    for i in range(2,(num)):
+        if (num%i == 0):
+            return False   
     return True
+
+letters = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l':12,'n':13,'o':14,'p':15,'q':16,'r':17,'s':18,'t':19,'u':20,'v':21,'w':22,'x':23,'y':24,'z':25}
 
 
 #Crear funcion que genere un numero primo de la longitud de bits que se requiera
@@ -24,4 +25,3 @@ def primalidad (num):
     #    num=numero
      #   break
         
-
