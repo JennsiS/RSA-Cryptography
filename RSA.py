ddda830f668139261349272d9ddce33ba9578a34
lquezada <que18028@uvg.edu.gt>
2019-11-11 23:41:04 -0600

    primes added and main funcs added

--- RSA.py
+++ RSA.py
@@ -24,7 +24,144 @@ def gcd(a,b):
     else: 
         return gcd(b,a%b)
 
+def xgcd(a, b):
+    x, old_x = 0, 1
+    y, old_y = 1, 0
+
+    while (b != 0):
+        quotient = a // b
+        a, b = b, a - quotient * b
+        old_x, x = x, old_x - quotient * x
+        old_y, y = y, old_y - quotient * y
+    return a, old_x, old_y
+
+def chooseE(totient):
+    while (True):
+        e = random.randrange(2, totient)
+        if (gcd(e, totient) == 1):
+            return e
+
+def chooseKeys():
+        # choose two random numbers within the range of lines where 
+        # the prime numbers are not too small and not too big
+        rand1 = random.randint(100, 300)
+        rand2 = random.randint(100, 300)
+
+        # store the txt file of prime numbers in a python list
+        fo = open('primes.txt', 'r')
+        lines = fo.read().splitlines()
+        fo.close()
+
+        # store our prime numbers in these variables
+        prime1 = int(lines[rand1])
+        prime2 = int(lines[rand2])
+
+        # compute n, totient, e
+        n = prime1 * prime2
+        totient = (prime1 - 1) * (prime2 - 1)
+        e = chooseE(totient)
+
+        # compute d, 1 < d < totient such that ed = 1 (mod totient)
+        # e and d are inverses (mod totient)
+        gcd, x, y = xgcd(e, totient)
+
+        # make sure d is positive
+        if (x < 0):
+            d = x + totient
+        else:
+            d = x
+
+        # write the public keys n and e to a file
+        f_public = open('public_keys.txt', 'w')
+        f_public.write(str(n) + '\n')
+        f_public.write(str(e) + '\n')
+        f_public.close()
+
+        f_private = open('private_keys.txt', 'w')
+        f_private.write(str(n) + '\n')
+        f_private.write(str(d) + '\n')
+        f_private.close()
+
+def encrypt(message, file_name = 'public_keys.txt', block_size = 2):
+    try:
+        fo = open(file_name, 'r')
+
+    # check for the possibility that the user tries to encrypt something
+    # using a public key that is not found
+    except FileNotFoundError:
+        print('That file is not found.')
+    else:
+        n = int(fo.readline())
+        e = int(fo.readline())
+        fo.close()
+
+        encrypted_blocks = []
+        ciphertext = -1
+
+        if (len(message) > 0):
+            # initialize ciphertext to the ASCII of the first character of message
+            ciphertext = ord(message[0])
+
+        for i in range(1, len(message)):
+            # add ciphertext to the list if the max block size is reached
+            # reset ciphertext so we can continue adding ASCII codes
+            if (i % block_size == 0):
+                encrypted_blocks.append(ciphertext)
+                ciphertext = 0
+
+            # multiply by 1000 to shift the digits over to the left by 3 places
+            # because ASCII codes are a max of 3 digits in decimal
+            ciphertext = ciphertext * 1000 + ord(message[i])
+
+        # add the last block to the list
+        encrypted_blocks.append(ciphertext)
+
+        # encrypt all of the numbers by taking it to the power of e
+        # and modding it by n
+        for i in range(len(encrypted_blocks)):
+            encrypted_blocks[i] = str((encrypted_blocks[i]**e) % n)
+
+        # create a string from the numbers
+        encrypted_message = " ".join(encrypted_blocks)
+
+        return encrypted_message
+
+def decrypt(blocks, block_size = 2):
+
+    fo = open('private_keys.txt', 'r')
+    n = int(fo.readline())
+    d = int(fo.readline())
+    fo.close()
+
+    # turns the string into a list of ints
+    list_blocks = blocks.split(' ')
+    int_blocks = []
+
+    for s in list_blocks:
+        int_blocks.append(int(s))
+
+    message = ""
+
+    # converts each int in the list to block_size number of characters
+    # by default, each int represents two characters
+    for i in range(len(int_blocks)):
+        # decrypt all of the numbers by taking it to the power of d
+        # and modding it by n
+        int_blocks[i] = (int_blocks[i]**d) % n
+
+        tmp = ""
+        # take apart each block into its ASCII codes for each character
+        # and store it in the message string
+        for c in range(block_size):
+            tmp = chr(int_blocks[i] % 1000) + tmp
+            int_blocks[i] //= 1000
+        message += tmp
+
+    return message
+
+
 # MAIN _______________________________________________________________
+
 parser = argparse.ArgumentParser()
 parser.add_argument('command',help='encrypt o decrypt')
 parser.add_argument('p',help='p')
@@ -32,33 +169,16 @@ parser.add_argument('q',help='q')
 parser.add_argument('msg',help='Mensaje a encriptar o decriptar')
 args = parser.parse_args()
 
-# Get the data
-command = args.command
-p = int(args.p)
-q = int(args.q)
-msg = args.msg
+chooseKeys()
 
-n = p * q 
-t = (p-1) * (q-1) 
+instruction = input('Would you like to encrypt or decrypt? (Enter e or d): ')
 
-for e in range(2,t): 
-    if gcd(e,t)== 1: 
-        break
-  
-for i in range(1,10): 
-    x = 1 + i*t 
-    if x % e == 0: 
-        d = int(x/e) 
-        break
-ctt = Decimal(0) 
-ctt =pow(no,e) 
-ct = ctt % n 
-  
-dtt = Decimal(0) 
-dtt = pow(ct,d) 
-dt = dtt % n 
-  
-print('n = '+str(n)+' e = '+str(e)+' t = '+str(t)+' d = '+str(d)+' cipher text = '+str(ct)+' decrypted text = '+str(dt)) 
+if (args.command == 'encrypt'):
+    print(encrypt(args.msg))
+
+elif (args.command == 'decrypt'):
+    print(decrypt(args.msg))
+
 
 
 
